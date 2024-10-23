d1a3665a2e665e4afd43aecddc7cf2d926889f46
dNILLnos2003 <danillanosarias@gmail.com>
2024-10-23 07:33:24 -0500

    Creación tiendas react

---
+++ 1-intro-react/1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  
+  return(
+    <div>
+      <header>Header
+        <nav>Menu de navegacion</nav>
+      </header>
+      <section>Seccion promos🍊</section>
+      <section>Seccion tienda🏣</section>
+      <footer>Pie de pagina</footer>
+    </div>
+  )
+    
+  }
+  
+  export default App
+  
