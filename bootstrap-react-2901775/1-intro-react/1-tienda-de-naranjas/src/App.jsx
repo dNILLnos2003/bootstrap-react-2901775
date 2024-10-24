09b9f2e3cbd9faf769330edaf9f10323fcb9f1a5
dNILLnos2003 <danillanosarias@gmail.com>
2024-10-24 07:32:55 -0500

    app Actualizado

---
+++ bootstrap-react-2901775/1-intro-react/1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from "./Componentes/Header"
+import Section from "./Componentes/Section"
+import Article from "./Componentes/Article"
+import Footer from "./Componentes/Footer"
+
+function App() {
+  
+  return(
+    <div>
+      <Header/>
+      <Section/>
+      <Article/>
+      <Footer/>
+    </div>
+  )
+    
+  }
+  
+  export default App
+  
---
+++ bootstrap-react-2901775/1-intro-react/1-tienda-de-naranjas/src/Componentes/Article.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Article() {
+  return (
+    <article>Articulo tienda 🏣</article>
+  )
+}
